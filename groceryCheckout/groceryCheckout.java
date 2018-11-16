d9da2048c40c2b6605d75bd9a2227949844102c0
Caleb Matsick <calebmatsick@gmail.com>
2018-11-16 13:49:30 -0500

    Updated documentation

--- groceryCheckout/groceryCheckout.java
+++ groceryCheckout/groceryCheckout.java
@@ -1,6 +1,7 @@
-//Calculates total cost of groceries, highest priced item, 
-//lowest priced item, and number of items entered
-
+/*
+ * Calculates total cost of groceries, highest priced item,
+ * lowest priced item, and number of items entered
+ */
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
@@ -15,19 +16,19 @@ public class groceryCheckout
    double highest = Double.MIN_VALUE;
    int itemNumber = 2;
    int totalItems = 0;
-   
+
    //Creates scanner and DecimalFormat objects
    Scanner userInput = new Scanner(System.in);
    DecimalFormat dollar = new DecimalFormat("$,##0.00");
-   
+
    //Gets userinput
    System.out.println("Enter the cost of Item #1 or -1 to finish");
       groceryCost = userInput.nextDouble();
-      
+
    //Lets the user know no items were entered if -1 was entered first
    if (groceryCost == -1)
       System.out.println("No items were entered");
-      
+
    //Finishes the rest of the program if items were entered
    else
       {
@@ -35,22 +36,22 @@ public class groceryCheckout
         while (groceryCost != -1)
         {
         totalCost += groceryCost;  //Adds individual cost to total
-          
-        //Finds the lowest and highest costs of the groceries       
+
+        //Finds the lowest and highest costs of the groceries
         if (lowest > groceryCost)
            lowest = groceryCost;
-        
+
         if (highest < groceryCost)
            highest = groceryCost;
-         
-        //Asks user for another item or to exit loop  
+
+        //Asks user for another item or to exit loop
         System.out.println("Enter the cost of Item #" + itemNumber + " cost or -1 to end");
            groceryCost = userInput.nextDouble();
 
         totalItems++; //Adds one to total items
         itemNumber++; //Adds one to item number
         }
-      
+
       //Displays total cost, highest priced item, lowest priced, and total items to user
       System.out.println("The number of items entered is " + totalItems);
       System.out.println("The lowest price item was " + dollar.format(lowest));
