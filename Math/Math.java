e701786db4aa0e5379ffe2847d42e20f12b8be32
Caleb Matsick <calebmatsick@gmail.com>
2018-12-03 13:29:40 -0500

    Added Math project

---
+++ Math/Math.java
@@ -0,0 +1,17 @@
+//Solves random math problems!
+
+public class Math
+{
+   public static void main(String[] args)
+   {
+   System.out.println ("A = " + (25 - (7 - 12 / 6) * 4));
+   
+   System.out.println ("B = " + (6 + 4 * 3 - 10 / 5));
+   
+   System.out.println ("C = " + (3 * 7 - 11 + 15 / 3));
+   
+   System.out.println ("D = " + ((18 / 3 + 28 / 4 + 2 * 4) / 3));
+   
+   System.out.println ("E = " + (3 * (((7 - 5) * (7 - 5)) + ((20 - 19) * (20 - 19))) + 14));   
+   }
+}
