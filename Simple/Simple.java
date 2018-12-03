5358af30c25e974eb87048c86ca48f365867f13c
Caleb Matsick <calebmatsick@gmail.com>
2018-12-03 13:30:18 -0500

    Added Simple project

---
+++ Simple/Simple.java
@@ -0,0 +1,10 @@
+// This is a simple Java Program
+
+public class Simple
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World");
+      System.out.println("I am Caleb and this is my first Java program");
+   }
+}
